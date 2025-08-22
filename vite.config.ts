b71cea7b07979e90c6663736baa71cd503163c3a
Karn2412 <eldhoraj33@gmail.com>
2025-08-22 10:28:02 +0530

    for now

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  esbuild: {
+    tsconfigRaw: {
+      compilerOptions: {
+        strict: false,
+        noUnusedLocals: false,
+        noUnusedParameters: false,
+        skipLibCheck: true
+      }
+    }
+  }
 })
